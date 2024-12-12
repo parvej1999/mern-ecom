db5cbc2466eb5e242f26ebbc2b217a68a8b0985b
parvej khan <1999monustp@gmail.com>
2024-12-12 20:19:54 +0530

    login endpoint added and working

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from '../models/userModel.js'
 import { hashPassword, comparePassword } from '../helpers/authHelper.js'
+import jwt from 'jsonwebtoken'
 
 export const registerUser = async (req, res) => {
     try {
@@ -11,7 +12,7 @@ export const registerUser = async (req, res) => {
         if (isExisting) {
             console.log("user already exists");
             return res.status(200).send({
-                success: true,
+                success: false,
                 message: "user already exists",
             });
         }
@@ -22,15 +23,62 @@ export const registerUser = async (req, res) => {
         });
         await user.save()
         return res.status(201).send({
-            sucess: true,
+            success: true,
             message: "user created",
             user,
         })
     }
     catch (error) {
-        send.status(500).send({
+        res.status(500).send({
             success: false,
             message: "encoutered some error",
+            error,
+        })
+    }
+}
+
+export const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: "Id Or Password Not Right",
+            })
+        }
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "User Does Not Exists",
+            })
+        }
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.status(200).send({
+                success: false,
+                message: "invalid user or password"
+            })
+        }
+        const token = await jwt.sign({ _id: user._id }, process.env.JWT_TOKEN, { expiresIn: "7d" });
+        console.log(`login ${match}`, token);
+        return res.status(200).send({
+            success: true,
+            message: "login successfull",
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                address: user.address,
+            },
+            token,
+        });
+    }
+    catch (error) {
+        return res.status(500).send({
+            success: false,
+            message: "some error encountered",
+            error,
         })
     }
 }
